2e94876dd879cdc1abbff7e8e8089b3d13eec4ed
kopchikovich <kopchikovich@yandex.ru>
2019-12-15 13:14:48 +1000

    Start to migrate old workout app on React

---
+++ src/components/app.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            screen: ''
+        }
+    }
+
+    render() {
+        return (
+            <div className='app'>
+            
+            </div>
+        )
+    }
+}
+
+export default App
