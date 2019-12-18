70748ed44b5ddb4cedcf50cae73e0bcb305ba130
kopchikovich <kopchikovich@yandex.ru>
2019-12-18 13:14:19 +1000

    Make timer component

--- src/components/screen-workout.js
+++ src/components/screen-workout.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import './screen-workout.css'
 import training_db from '../data'
+import Timer from './timer'
 
 class ScreenWorkout extends Component {
 
@@ -9,10 +10,29 @@ class ScreenWorkout extends Component {
     }
 
     render() {
+
         const training = training_db[this.props.state.trainingKey];
-        console.log(training)
+
         return (
-            <h2>{training.description} </h2>
+            <section className='training-table'>
+
+                <article className='training-table__cell training-table__cell--timer timer'>
+                    <Timer />
+                </article>
+
+                <article className='training-table__cell training-table__cell--timer timer'>
+                    <Timer />
+                </article>
+
+                <article className='training-table__cell training-table__cell--exercise exercise'>
+
+                </article>
+
+                <article className='training-table__cell training-table__cell--sets sets'>
+
+                </article>
+
+            </section>
         )
     }
 }
---
+++ src/components/timer.js
@@ -0,0 +1,49 @@
+import React, {Component} from 'react'
+
+class Timer extends Component {
+
+    initialState = {
+        minutes: this.props.minutes? this.props.minutes : '00',
+        seconds: this.props.seconds? this.props.seconds : '00'
+    }
+    state = this.initialState
+
+    tick() {
+        let minutes = +this.state.minutes;
+        let seconds = +this.state.seconds;
+        seconds++;
+            if (seconds === 60) {
+                seconds = 0;
+                minutes++;
+            }
+            if (minutes === 60) minutes = 0;
+        this.setState({
+            minutes: minutes < 10? '0' + minutes: minutes,
+            seconds: seconds < 10? '0' + seconds: seconds
+        })
+    }
+
+    componentDidMount() {
+        this.timerInterval = setInterval(this.tick.bind(this), 1000);
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerInterval);
+    }
+
+    render() {
+        return (
+            <div className='timer__time'>
+                <span className='timer__minutes'>
+                    {this.state.minutes}
+                </span>
+                :
+                <span className='timer__seconds'>
+                    {this.state.seconds}
+                </span>
+            </div>
+        )
+    }
+}
+
+export default Timer
