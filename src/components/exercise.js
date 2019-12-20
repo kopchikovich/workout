503ea778edcf0991d077da26c03467535599f598
kopchikovich <kopchikovich@yandex.ru>
2019-12-20 14:14:37 +1000

    Replace state to parent component

--- src/components/exercise.js
+++ src/components/exercise.js
@@ -1,60 +1,29 @@
-import React, {Component} from 'react'
+import React from 'react'
 import './exercise.css'
 import Button from './button'
 
-class Exercise extends Component {
+const Exercise = (props) => {
 
-    constructor(props) {
-        super(props)
+    const exercise = props.state.currentExs;
 
-        this.initialState = {
-            currentExs: this.props.training.exercises[0],
-            currentExsIndex: 0
-        }
-        this.state = this.initialState;
-
-        this.training = this.props.training;
-    }
-
-    render() {
-        return (
-            <>
-                <h3 className='exercise__header'>Упражнение</h3>
-                <div className='exercise__checker checker input__container'>
-                    <Button className='checker__btn' title='<' value='prev' onClickHandler={this.switchExercise.bind(this)} />
-                    <span className='checker__current-exs'>
-                        {this.state.currentExs.name}
-                    </span>
-                    <Button className='checker__btn' title='>' value='next' onClickHandler={this.switchExercise.bind(this)} />
-                </div>
-                <p className='exercise__sets'>
-                    Подходы: <span>{this.state.currentExs.sets}</span>
-                </p>
-                <form className='exercise__input input' name='exercise-form'>
-                    <Button className='input__btn-submit' title='Записать' onClickHandler={null} />
-                </form>
-            </>
-        )
-    }
-
-    switchExercise(e) {
-        let newExs = null;
-        let newExsIndex = 0;
-
-        if (e.target.value === 'prev') {
-            newExsIndex = this.state.currentExsIndex-1 <= 0? 0 : this.state.currentExsIndex-1;
-        } else if (e.target.value === 'next') {
-            newExsIndex = this.state.currentExsIndex+1 >= this.training.exercises.length-1? this.training.exercises.length-1 : this.state.currentExsIndex+1;
-        } else {
-            return null;
-        }
-        newExs = this.training.exercises[newExsIndex];
-
-        this.setState({
-            currentExs: newExs,
-            currentExsIndex: newExsIndex
-        })
-    }
+    return (
+        <>
+            <h3 className='exercise__header'>Упражнение</h3>
+            <div className='exercise__checker checker input__container'>
+                <Button className='checker__btn' title='<' value='prev' onClickHandler={props.switchExercise} />
+                <span className='checker__current-exs'>
+                    {exercise.name}
+                </span>
+                <Button className='checker__btn' title='>' value='next' onClickHandler={props.switchExercise} />
+            </div>
+            <p className='exercise__sets'>
+                Подходы: <span>{exercise.sets}</span>
+            </p>
+            <form className='exercise__input input' name='exercise-form'>
+                <Button className='input__btn-submit' title='Записать' onClickHandler={null} />
+            </form>
+        </>
+    )
 }
 
 export default Exercise
--- src/components/screen-workout.js
+++ src/components/screen-workout.js
@@ -7,14 +7,20 @@ import Sets from './sets'
 
 class ScreenWorkout extends Component {
 
-    state = {
+    constructor(props) {
+        super(props)
+
+        this.training = training_db[this.props.state.trainingKey];
+
+        this.initialState = {
+            currentExs: this.training.exercises[0],
+            currentExsIndex: 0
+        }
+        this.state = this.initialState;
 
     }
 
     render() {
-
-        const training = training_db[this.props.state.trainingKey];
-
         return (
             <section className='training-table'>
 
@@ -27,7 +33,7 @@ class ScreenWorkout extends Component {
                 </article>
 
                 <article className='training-table__cell training-table__cell--exercise exercise'>
-                    <Exercise training={training} />
+                    <Exercise state={this.state} training={this.training} switchExercise={this.switchExercise.bind(this)} />
                 </article>
 
                 <article className='training-table__cell training-table__cell--sets sets'>
@@ -36,6 +42,25 @@ class ScreenWorkout extends Component {
 
             </section>
         )
+    }
+
+    switchExercise(e) {
+        let newExs = null;
+        let newExsIndex = 0;
+
+        if (e.target.value === 'prev') {
+            newExsIndex = this.state.currentExsIndex-1 <= 0? 0 : this.state.currentExsIndex-1;
+        } else if (e.target.value === 'next') {
+            newExsIndex = this.state.currentExsIndex+1 >= this.training.exercises.length-1? this.training.exercises.length-1 : this.state.currentExsIndex+1;
+        } else {
+            return null;
+        }
+        newExs = this.training.exercises[newExsIndex];
+
+        this.setState({
+            currentExs: newExs,
+            currentExsIndex: newExsIndex
+        })
     }
 }
 
