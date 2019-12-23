dfdb3e6b73e9dce7369999ae06839920404648bc
kopchikovich <kopchikovich@yandex.ru>
2019-12-23 15:12:53 +1000

    Make readable sets

--- src/components/sets.js
+++ src/components/sets.js
@@ -1,12 +1,32 @@
 import React from 'react'
-import './exercise.css'
+import './sets.css'
 
 const Sets = (props) => {
+
+    const makeSetString = (set) => {
+        const options = Object.keys(set);
+        const GRAM_IN_KILOGRAM = 1000;
+
+        if (options.includes('time-left') && options.includes('time-right')) {
+            return `лев - ${set['time-left']} / прав - ${set['time-right']}`;
+        } else if (options.includes('weight') && options.includes('repeats-left') && options.includes('repeats-right')) {
+            return `лев ${(set.weight/GRAM_IN_KILOGRAM).toFixed(1)}кг - ${set['repeats-left']} / прав ${(set.weight/GRAM_IN_KILOGRAM).toFixed(1)}кг - ${set['repeats-right']}`;
+        } else if (options.includes('repeats-left') && options.includes('repeats-right')) {
+            return `лев - ${set['repeats-left']} / прав - ${set['repeats-right']}`;
+        } else if (options.includes('time')) {
+            return `${set.time}`;
+        } else if (options.includes('weight') && options.includes('repeats')) {
+            return `${(set.weight/GRAM_IN_KILOGRAM).toFixed(1)} кг - ${set.repeats}`;
+        } else if (options.includes('repeats')) {
+            return `${set.repeats}`;
+        }
+        return 'error';
+    }
 
     const renderSets = (set, index) => {
         return (
             <li className='sets__set' key={index}>
-                {set.toString()}
+                {makeSetString(set)}
             </li>
         )
     }
