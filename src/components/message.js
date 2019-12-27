ad90981535608f9de1f96fa5318b6544437bc83a
kopchikovich <kopchikovich@yandex.ru>
2019-12-27 16:52:35 +1000

    Add color picker from props

--- src/components/message.js
+++ src/components/message.js
@@ -3,9 +3,11 @@ import './message.css'
 const makeMessage = (text, color) => {
 
     const message = document.createElement('div');
+    const renderColor = color === 'green'? '#0a0' : '#a00';
+
     message.className = 'message';
     message.innerHTML = text;
-    message.style.color = color;
+    message.style.color = renderColor;
     
     return message;
 }
