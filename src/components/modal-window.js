10179dae413d7592ff29eadf9cf75e6b3a27858e
kopchikovich <kopchikovich@yandex.ru>
2019-12-27 14:26:38 +1000

    Add hidden header if need

--- src/components/modal-window.js
+++ src/components/modal-window.js
@@ -4,11 +4,12 @@ import './modal-window.css'
 const ModalWindow = (props) => {
 
         let display = props.isVisible? 'flex' : 'none';
+        let displayHeader = props.header? 'flex' : 'none';
 
         return (
             <div className='modal__wrapper' style={{display: display}} onClick={props.closeModal}>
                 <section className='modal'>
-                    <header className='modal__header'>
+                    <header className='modal__header' style={{display: displayHeader}}>
                         {props.header}
                     </header>
                     <article className='modal__content'>
