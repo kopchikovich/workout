2a24f875b1b64da84938a7a5c7cdf3642c09f448
kopchikovich <kopchikovich@yandex.ru>
2019-12-29 11:40:21 +1000

    Remove login from this screen

--- src/components/screens/screen-user.js
+++ src/components/screens/screen-user.js
@@ -1,15 +1,11 @@
 import React from 'react'
-import Login from '../login'
 import User from '../user'
 
 const ScreenUser = (props) => {
 
-    const loginView = <Login login={props.login} />
-    const userView = <User logout={props.logout} />
-
     return (
         <section>
-            {props.isLogin? userView : loginView}
+            <User logout={props.logout} />
         </section>
     )
 }
