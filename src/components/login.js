9f16ef707524165e8a9a5c939998bb012288841d
kopchikovich <kopchikovich@yandex.ru>
2019-12-29 11:37:56 +1000

    Make login screen full screen

--- src/components/login.js
+++ src/components/login.js
@@ -4,17 +4,19 @@ import Button from './button'
 
 const Login = (props) => {
     return (
-        <form className='login' onSubmit={props.login}>
-            <label className='login__block'>
-                <span className='login__text'>Почта</span>
-                <input type='email' name='email' id='email' className='login__input' />
-            </label>
-            <label className='login__block'>
-                <span className='login__text'>Пароль</span>
-                <input type='password' name='password' id='password' className='login__input' />
-            </label>
-            <Button className='login__submit' title='Войти' />
-        </form>
+        <div className='login__wrapper'>
+            <form className='login' onSubmit={props.login}>
+                <label className='login__block'>
+                    <span className='login__text'>Почта</span>
+                    <input type='email' name='email' id='email' className='login__input' />
+                </label>
+                <label className='login__block'>
+                    <span className='login__text'>Пароль</span>
+                    <input type='password' name='password' id='password' className='login__input' />
+                </label>
+                <Button className='login__submit' title='Войти' />
+            </form>
+        </div>
     )
 }
 
