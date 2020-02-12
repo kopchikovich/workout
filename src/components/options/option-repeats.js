fc9e8e43f1d6293492a11a43f976185adde266e8
kopchikovich <kopchikovich@yandex.ru>
2020-02-12 21:01:30 +1000

    Change defaultValue -> placeholder

--- src/components/options/option-repeats.js
+++ src/components/options/option-repeats.js
@@ -4,7 +4,7 @@ const OptionRepeats = () => {
     return (
         <label className='input__container'>
             <span className='input__text'>Повторы</span>
-            <input type='number' className='input__input input__input--repeats' name='repeats' defaultValue='0' />
+            <input type='number' className='input__input input__input--repeats' name='repeats' placeholder='0' />
         </label>
     )
 }
