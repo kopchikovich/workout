340314245f469416d2b650301facb9ddbeb3c811
kopchikovich <kopchikovich@yandex.ru>
2020-02-18 18:25:52 +1000

    Change defaultValue -> placeholder in inputs

--- src/components/modal-form.js
+++ src/components/modal-form.js
@@ -13,11 +13,11 @@ const ModalForm = (props) => {
         <form className='modal__form' onSubmit={submit}>
             <div className='modal__container'>
                 <span className='modal__text'>Время (минут)</span>
-                <input className='modal__input' name='duration' type='number' min='1' max='120' step='1' defaultValue='0' />
+                <input className='modal__input' name='duration' type='number' min='1' max='120' step='1' placeholder='0' />
             </div>
             <div className='modal__container'>
                 <span className='modal__text'>Расстояние (м)</span>
-                <input className='modal__input' name='distance' type='number' min='100' max='15000' step='100' defaultValue='0' />
+                <input className='modal__input' name='distance' type='number' min='100' max='15000' step='100' placeholder='0' />
             </div>
             <Button className='modal__submit' title='Записать' />
         </form>
