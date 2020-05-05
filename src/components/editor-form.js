72d89a1e0ff11bda15583495e9d7edb233a725a5
kopchikovich <kopchikovich@yandex.ru>
2020-05-05 19:02:10 +1000

    Add name and description fields to editor form

--- src/components/editor-form.js
+++ src/components/editor-form.js
@@ -1,14 +1,45 @@
 import React from 'react'
 import './editor-form.css'
+import Button from './button'
 
 const EditorForm = (props) => {
 
     const [ editableItemId, editableItem ] = Object.entries(props.targetObj).find((el) => el[1].name === props.target)
 
+    const submitHandler = (e) => {
+        e.preventDefault()
+        console.log('submit', editableItemId);
+    }
+
     return (
-        <form className='editor-form'>
-            <h2>{editableItem.name}</h2>
-            <p>{editableItem.description}</p>
+        <form className='editor-form' onSubmit={submitHandler}>
+            <label className='editor-form__label'>
+                <span className='editor-form__text'>
+                    Название
+                </span>
+                <input
+                    className='editor-form__text-input'
+                    type='text'
+                    id='name'
+                    value={editableItem.name}
+                    onChange={(e) => console.log(e)}
+                />
+            </label>
+            <label className='editor-form__label'>
+                <span className='editor-form__text'>
+                    Описание
+                </span>
+                <textarea
+                    className='editor-form__textarea'
+                    id='description'
+                    value={editableItem.description}
+                    onChange={(e) => console.log(e)}
+                />
+            </label>
+            <Button
+                className='editor-form__button'
+                title='Сохранить'
+            />
         </form>
     )
 }
